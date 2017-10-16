15eb135af68736910e9820940ee2a13255e3499d
primo80 <32191939+primo80@users.noreply.github.com>
2017-10-16 11:18:43 +0200

    modulo primero de git hub

---
+++ Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim temperatura, presion As Double
+
+        Console.WriteLine("Dime la temperatura")
+        temperatura = Console.ReadLine
+        Console.WriteLine("Dime la presion")
+        presion = Console.ReadLine
+
+        If temperatura > 500 Then
+            Console.WriteLine("reducir la temperatura")
+        End If
+
+
+
+        If presion > 2 Then
+            Console.WriteLine("abrir valvula de seguridad")
+        End If
+
+
+
+
+        If presion <= 2 And temperatura < 500 Then
+            Console.WriteLine("todo en orden")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
